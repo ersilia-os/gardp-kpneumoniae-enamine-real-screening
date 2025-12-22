a32c9f59c240f4dd088fb71b401527fd7aef3a3e
miquelduranfrigola <miquelduranfrigola@gmail.com>
2025-12-22 11:57:50 +0100

    download and h5 conversion

--- src/core/utils.py
+++ src/core/utils.py
@@ -1,11 +1,15 @@
 import os
 import h5py
+import time
 import httplib2
 import pandas as pd
 import numpy as np
-from scipy.sparse import load_npz
+from tqdm import tqdm
 from google.oauth2.service_account import Credentials
 from googleapiclient.discovery import build
+
+GDRIVE_FOLDER_ID = "1FBELagBf9hlKVgvkaZ8YF60jKRAmsHPo"
+ARRAY_DTYPE = "int8"
 
 
 def _get_data_file(dir_path, chunk_name):
@@ -13,7 +17,7 @@ def _get_data_file(dir_path, chunk_name):
 
 
 def _get_identifiers_file(dir_path, chunk_name):
-    return os.path.join(dir_path, f"{chunk_name}_SMILES_IDs.csv.zip")
+    return os.path.join(dir_path, f"{chunk_name}_SMILES_IDs.tsv.zip")
 
 
 def _get_h5_file(dir_path, chunk_name):
@@ -27,12 +31,13 @@ def check_exists(dir_path, chunk_name):
         return True
     else:
         return False
-    
+
+
 def download_file(outfile):
     root = os.path.dirname(os.path.abspath(__file__))
     file = os.path.basename(outfile)
-    service_file = os.path.abspath(os.path.join(root, "..", "..", "data", "service", "service.json"))
-    folder_id = "19x9yAUySBXgrHBE3gjGjHsomcLQuLQmL"
+    service_file = os.path.abspath(os.path.join(root, "..", "..", "config", "service.json"))
+    folder_id = GDRIVE_FOLDER_ID
     creds = Credentials.from_service_account_file(service_file, scopes=["https://www.googleapis.com/auth/drive.readonly"])
     service = build("drive", "v3", credentials=creds)
     httplib2.DEFAULT_TIMEOUT = 600
@@ -53,6 +58,7 @@ def download_file(outfile):
 
 
 def download_data(dir_path, chunk_name):
+    t0 = time.time()
     if not os.path.exists(dir_path):
         os.makedirs(dir_path)
     data_file = _get_data_file(dir_path, chunk_name)
@@ -65,27 +71,34 @@ def download_data(dir_path, chunk_name):
     download_file(identifiers_file)
     print(f"Downloading data_file to {data_file}...")
     download_file(data_file)
+    t1 = time.time()
+    print(f"Download completed in {t1 - t0:.2f} seconds.")
 
 
 def convert_to_h5(dir_path, chunk_name, batch_size=100_000):
+    t0 = time.time()
+    print(f"Converting chunk {chunk_name} to H5 format...")
     data_file = _get_data_file(dir_path, chunk_name)
     identifiers_file = _get_identifiers_file(dir_path, chunk_name)
     h5_file = _get_h5_file(dir_path, chunk_name)
-    X_sparse = load_npz(data_file)   # csr_matrix
-    n_rows, n_cols = X_sparse.shape
-    identifiers = pd.read_csv(identifiers_file, compression="zip")
-    print(len(identifiers), n_rows)
+    identifiers = pd.read_csv(identifiers_file, compression="zip", delimiter="\t")
+    print(identifiers.head())
+    X = np.load(data_file)["X"]
+    n_rows, n_cols = X.shape
+    print(f"Identifiers rows: {len(identifiers)}, Data shape: {n_rows}, {n_cols}")
     assert len(identifiers) == n_rows, "Row mismatch between X and identifiers"
     with h5py.File(h5_file, "w") as f:
-        dset_X = f.create_dataset("values", shape=(n_rows, n_cols), dtype="int8", chunks=(batch_size, n_cols), compression="gzip")
-        f.create_dataset("key", data=np.array(identifiers["IDS"]).astype("S"))
-        f.create_dataset("input", data=np.array(identifiers["SMILES"]).astype("S"))
-        for start in range(0, n_rows, batch_size):
+        dset_X = f.create_dataset("values", shape=(n_rows, n_cols), dtype=ARRAY_DTYPE, chunks=(batch_size, n_cols), compression="gzip")
+        f.create_dataset("key", data=identifiers["id"].tolist(), dtype=h5py.string_dtype())
+        f.create_dataset("input", data=identifiers["smiles"].tolist(), dtype=h5py.string_dtype())
+        for start in tqdm(range(0, n_rows, batch_size), desc="Converting to H5"):
             end = min(start + batch_size, n_rows)
-            print(f"Writing rows {start}:{end}")
-            X_batch = X_sparse[start:end].toarray().astype("int8")
+            X_batch = X[start:end].astype(ARRAY_DTYPE)
             dset_X[start:end] = X_batch
             del X_batch
+    t1 = time.time()
+    print(f"H5 file saved to {h5_file}")
+    print(f"Conversion completed in {t1 - t0:.2f} seconds.")
     return h5_file
 
 
--- src/run.py
+++ src/run.py
@@ -15,10 +15,11 @@ def main():
     chunk_name = args.chunk_name
     output_dir = args.output_dir
 
-    download_data(output_dir, chunk_name)
-    h5_file = convert_to_h5(output_dir, chunk_name)
-    #screener = LightScreener()
-    #screener.screen(h5_input=h5_file, csv_output=os.path.join(output_dir, f"{chunk_name}_hits.csv"))
+    #download_data(output_dir, chunk_name)
+    #h5_file = convert_to_h5(output_dir, chunk_name)
+    h5_file = os.path.join(output_dir, f"{chunk_name}.h5") # TODO remove when debugged
+    screener = LightScreener()
+    screener.screen(h5_input=h5_file, csv_output=os.path.join(output_dir, f"{chunk_name}_hits.csv"))
     #clean_data(output_dir, chunk_name)
 
 
