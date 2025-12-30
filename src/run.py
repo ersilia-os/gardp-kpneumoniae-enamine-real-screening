6b69202ee022d1646cde6963fa3d67000e1c4b39
Miquel Duran-Frigola <miquelduranfrigola@gmail.com>
2025-12-30 08:12:07 +0100

    skip clean

--- src/run.py
+++ src/run.py
@@ -10,19 +10,23 @@ def main():
     parser.add_argument("--chunk_name", type=str, required=True, help="Name of the data chunk to process.")
     parser.add_argument("--output_dir", type=str, required=True, help="Directory to save the output CSV file.")
     parser.add_argument("--from_dir", type=str, default=None, required=False, help="Directory to fetch fingerprints from. If provided, it skips downloading.")
+    parser.add_argument("--skip_clean", action='store_true', help="If set, skips the cleaning step after processing.")
     args = parser.parse_args()
 
     chunk_name = args.chunk_name
     output_dir = args.output_dir
     from_dir = args.from_dir
-    
+    skip_clean = args.skip_clean
+
     if from_dir is None:
         download_data(output_dir, chunk_name)
     else:
         copy_data(from_dir, output_dir, chunk_name)
     convert_to_h5(output_dir, chunk_name)
     pipeline(output_dir, chunk_name)
-    clean_data(output_dir, chunk_name)
+    
+    if not skip_clean:
+        clean_data(output_dir, chunk_name)
 
 
 if __name__ == "__main__":
